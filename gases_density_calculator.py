7d49586d41f9efd48ddcb37f9c24b5031f83f77d
Hardmat <62304951+Hardmat@users.noreply.github.com>
2023-06-06 11:02:15 -0400

    Add files via upload

--- gases_density_calculator.py
+++ gases_density_calculator.py
@@ -4,66 +4,50 @@ import numpy as np
 
 # Constants
 gas_molar_masses = {
-    "Nitrogen": 28.0134,  # g/mol
-    "Oxygen": 31.9988,  # g/mol
-    "Hydrogen": 2.01588,  # g/mol
-    "Carbon Dioxide": 44.0095,  # g/mol
-    "Argon": 39.948,  # g/mol
-    "Acetylene": 26.0373,  # g/mol
-    "Ammonia": 17.0306,  # g/mol
-    "Chlorine": 70.906,  # g/mol
-    "Sulfur Dioxide": 64.0638,  # g/mol
-    "Methane": 16.0425  # g/mol
+    "Oxygen": 32,  # g/mol
+    "Nitrogen": 28.0134,
+    "Hydrogen": 2.01588,
+    "Helium": 4.002602,
+    "Carbon Dioxide": 44.0095,
+    "Argon": 39.948,
+    "Methane": 16.04246,
+    "Ethane": 30.06904,
+    "Propane": 44.09562,
+    "Butane": 58.12218
 }
 
 ideal_gas_constant = 0.0821  # atm * L / (mol * K)
 molar_volume_conversion = 1000  # L/m^3
 
-# Function to calculate density of gas mixture
-def calculate_density(pressure, temperature, gas_mixture):
-    total_molar_mass = 0
-    total_molar_volume = 0
-
-    for gas, percentage in gas_mixture.items():
-        molar_mass = gas_molar_masses[gas]
-        molar_volume = (percentage / 100) * (molar_mass / ideal_gas_constant) * (temperature + 273.15) / pressure
-        total_molar_mass += molar_mass
-        total_molar_volume += molar_volume
-
-    density = (pressure * total_molar_mass) / (total_molar_volume * ideal_gas_constant)
+# Function to calculate density
+def calculate_density(gas, pressure, temperature):
+    molar_mass = gas_molar_masses[gas]
+    molar_mass_kg = molar_mass / 1000  # kg/mol
+    temperature_kelvin = temperature + 273.15  # Convert temperature to Kelvin
+    pressure_atm = pressure / 14.6959488  # Convert pressure from PSI to atm
+    density = (pressure_atm * molar_mass_kg) / (ideal_gas_constant * temperature_kelvin)
     density = density * molar_volume_conversion  # Convert from g/L to kg/m^3
     return density
 
 # Streamlit app
-st.title("Gas Mixture Density Calculator")
+st.title("Gaseous Density Calculator")
 
-# Gas mixture input
-gas_mixture = {}
-total_percentage = 0
-
-while total_percentage < 100:
-    remaining_percentage = 100 - total_percentage
-    st.write(f"Remaining Percentage: {remaining_percentage}%")
-
-    available_gases = [gas for gas in gas_molar_masses.keys() if gas not in gas_mixture]
-    selected_gas = st.selectbox("Select Gas", available_gases)
-    percentage = st.number_input(f"Percentage of {selected_gas}", min_value=0, max_value=remaining_percentage, step=1)
-
-    if selected_gas in gas_mixture:
-        st.warning("Gas already selected. Please choose a different gas.")
-        continue
-
-    gas_mixture[selected_gas] = percentage
-    total_percentage += percentage
-
-    if total_percentage == 100:
-        break
-
+gas = st.selectbox("Select Gas", list(gas_molar_masses.keys()))
 pressure = st.slider("Pressure (PSI)", 0, 200, step=1)
 temperature = st.slider("Temperature (°C)", 0, 80, step=1)
 
-density = calculate_density(pressure, temperature, gas_mixture)
-st.write("Density of Gas Mixture:", density, "kg/m^3")
+density = calculate_density(gas, pressure, temperature)
+st.write("Density of", gas, ":", density, "kg/m^3")
+
+# Generate pressure and temperature values
+pressure_values = np.linspace(0, 200, 100)
+temperature_values = np.linspace(0, 80, 100)
+
+# Calculate density for each pressure and temperature combination
+density_values = np.zeros((100, 100))
+for i, pressure_val in enumerate(pressure_values):
+    for j, temperature_val in enumerate(temperature_values):
+        density_values[i, j] = calculate_density(gas, pressure_val, temperature_val)
 
 
 # Generate pressure and temperature values for the 3D plot
