caaee05decdad9de8d8541266b18f4879a701f86
Hardmat <62304951+Hardmat@users.noreply.github.com>
2023-06-06 13:01:18 -0400

    Add files via upload

--- gases_density_calculator.py
+++ gases_density_calculator.py
@@ -1,8 +1,5 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import mpld3
-import mpld3.plugins as plugins
+import plotly.graph_objects as go
 import numpy as np
 
 # Constants
@@ -23,74 +20,64 @@ ideal_gas_constant = 0.0821  # atm * L / (mol * K)
 molar_volume_conversion = 1000  # L/m^3
 
 # Function to calculate density
-def calculate_density(gas_mixture, pressure, temperature):
-    total_percentage = sum(gas_mixture.values())
-    if total_percentage != 100:
-        st.error("Gas mixture percentages must add up to 100%.")
-        return None
-    density = 0
-    for gas, percentage in gas_mixture.items():
-        molar_mass = gas_molar_masses[gas]
-        molar_mass_kg = molar_mass / 1000  # kg/mol
-        mole_fraction = percentage / total_percentage
-        temperature_kelvin = temperature + 273.15  # Convert temperature to Kelvin
-        pressure_atm = pressure / 14.6959488  # Convert pressure from PSI to atm
-        partial_density = (pressure_atm * molar_mass_kg) / (ideal_gas_constant * temperature_kelvin)
-        density += partial_density * mole_fraction
+def calculate_density(gas, pressure, temperature):
+    molar_mass = gas_molar_masses[gas]
+    molar_mass_kg = molar_mass / 1000  # kg/mol
+    temperature_kelvin = temperature + 273.15  # Convert temperature to Kelvin
+    pressure_atm = pressure / 14.6959488  # Convert pressure from PSI to atm
+    density = (pressure_atm * molar_mass_kg) / (ideal_gas_constant * temperature_kelvin)
     density = density * molar_volume_conversion  # Convert from g/L to kg/m^3
     return density
 
 # Streamlit app
 st.title("Gaseous Density Calculator")
 
-gas_mixture = {}
-selected_gases = st.multiselect("Select Gases", list(gas_molar_masses.keys()))
-for gas in selected_gases:
-    percentage = st.number_input(f"Percentage of {gas}", min_value=0, max_value=100, key=gas)
-    gas_mixture[gas] = percentage
-
+gas = st.selectbox("Select Gas", list(gas_molar_masses.keys()))
 pressure = st.slider("Pressure (PSI)", 0, 200, step=1)
 temperature = st.slider("Temperature (°C)", 0, 80, step=1)
 
-density = calculate_density(gas_mixture, pressure, temperature)
+density = calculate_density(gas, pressure, temperature)
+st.write("Density of", gas, ":", density, "kg/m^3")
 
-if density is not None:
-    st.write("Density of Gas Mixture:", density, "kg/m^3")
+# Generate pressure and temperature values for the 3D plot
+pressure_values = np.linspace(0, 200, 100)
+temperature_values = np.linspace(0, 80, 100)
 
-    # Generate pressure and temperature values for the 3D plot
-    pressure_values = np.linspace(0, 200, 100)
-    temperature_values = np.linspace(0, 80, 100)
+# Calculate density for each pressure and temperature combination
+density_values = np.zeros((100, 100))
+for i, pressure_val in enumerate(pressure_values):
+    for j, temperature_val in enumerate(temperature_values):
+        density_values[i, j] = calculate_density(gas, pressure_val, temperature_val)
 
-    # Calculate density for each pressure and temperature combination
-    density_values = np.zeros((100, 100))
-    for i, pressure_val in enumerate(pressure_values):
-        for j, temperature_val in enumerate(temperature_values):
-            density_values[i, j] = calculate_density(gas_mixture, pressure_val, temperature_val)
+# Create a meshgrid for the pressure and temperature values
+pressure_grid, temperature_grid = np.meshgrid(pressure_values, temperature_values)
 
-    # Create a meshgrid for the pressure and temperature values
-    pressure_grid, temperature_grid = np.meshgrid(pressure_values, temperature_values)
+# Create a 3D plot of density using Plotly
+fig = go.Figure(data=[go.Surface(z=density_values, x=pressure_grid, y=temperature_grid)])
+fig.update_layout(scene=dict(
+    xaxis_title="Pressure (PSI)",
+    yaxis_title="Temperature (°C)",
+    zaxis_title="Density (kg/m^3)",
+    camera=dict(
+        eye=dict(x=1.5, y=1.5, z=1),
+        projection=dict(type="orthographic")
+    )
+))
 
-    # Create a 3D plot of density
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
-    ax.plot_surface(pressure_grid, temperature_grid, density_values, cmap="viridis", alpha=0.7)
-    ax.set_xlabel("Pressure (PSI)")
-    ax.set_ylabel("Temperature (°C)")
-    ax.set_zlabel("Density (kg/m^3)")
+# Add a marker for the calculated density position
+fig.add_trace(go.Scatter3d(
+    x=[pressure],
+    y=[temperature],
+    z=[density],
+    mode="markers",
+    marker=dict(
+        size=5,
+        color="red"
+    )
+))
 
-    # Set the title of the graph based on the gas mixture
-    gas_mixture_title = ", ".join([f"{gas} ({percentage}%)" for gas, percentage in gas_mixture.items()])
-    ax.set_title(f"Gas Mixture Density: {gas_mixture_title}")
+# Set the opacity of the plot
+fig.update_traces(opacity=0.8)
 
-    # Add a red dot for the calculated density position
-    ax.scatter(pressure, temperature, density, color="red", s=50)
-
-    # Enable rotation plugin for the 3D plot
-    plugins.connect(fig, plugins.MousePosition(fontsize=14))
-    plugins.connect(fig, plugins.RotateAxes3d())
-
-    # Generate the interactive plot using mpld3
-    interactive_plot = mpld3.fig_to_html(fig)
-
-    # Display the interactive plot
-    st.write(interactive_plot, unsafe_allow_html=True)
+# Display the plot using st.plotly_chart
+st.plotly_chart(fig)
